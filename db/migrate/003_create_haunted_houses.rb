f30f4274f65a1cb729a9df912bc8c48c03d0d97a
Dani Lyn <dlyn1110@gmail.com>
2018-07-06 10:57:06 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,11 +6,12 @@ class CreateHauntedHouses < ActiveRecord::Migration
         create_table :costume_stores do |t|
           t.string :name
           t.string :location
-          t.integer :costume_inventory
-          t.integer :num_of_employees
-          t.boolean :still_in_business
-          t.datetime :start_time
-          t.datetime :end_time
+          t.string :theme
+          t.float :price
+          t.boolean :family_friendly
+          t.datetime :opening_date
+          t.datetime :closing_date
+          t.string :description
     end
    end
   end
