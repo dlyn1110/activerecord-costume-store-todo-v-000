8273959733395771383cecaa335ecfc33912fed1
Dani Lyn <dlyn1110@gmail.com>
2018-07-06 11:02:40 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,5 +1,5 @@
 class CreateHauntedHouses < ActiveRecord::Migration
-  
+
   def change
     create_table :haunted_houses do |t|
           t.string :name
@@ -7,9 +7,9 @@ class CreateHauntedHouses < ActiveRecord::Migration
           t.string :theme
           t.float :price
           t.boolean :family_friendly
-          t.string :opening_date
-          t.string :closing_date
-          t.string :description
+          t.datetime :opening_date
+          t.datetime :closing_date
+          t.text :description
     end
    end
   end
