8a0ba5cec662a873389d14641902950b76bb1546
Dani Lyn <dlyn1110@gmail.com>
2018-07-06 10:35:50 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,6 +2,10 @@ class CreateCostumes < ActiveRecord::Migration
   def change
     create_table :costumes do |t|
       t.string :name
+      t.float :price
+      t.string :image_url
+      t.integer :size
+      t.timestamps
   end
  end
 end
