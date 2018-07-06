705130afda42f670d654a9e278c052a74893a72e
Dani Lyn <dlyn1110@gmail.com>
2018-07-06 10:52:31 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :start_time
-      t.datetime :closing_time
+      t.datetime :end_time
   end
  end
 end
