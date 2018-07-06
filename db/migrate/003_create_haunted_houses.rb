efe3bdd3e6b4a17c7facda77087e43d769abee65
Dani Lyn <dlyn1110@gmail.com>
2018-07-06 11:00:34 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,9 +1,6 @@
 class CreateHauntedHouses < ActiveRecord::Migration
   def change
     create_table :haunted_houses do |t|
-      t.string :name
-      def change
-        create_table :haunted_houses do |t|
           t.string :name
           t.string :location
           t.string :theme
